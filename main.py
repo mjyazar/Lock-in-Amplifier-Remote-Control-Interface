77d8b88f6932076280624bb7876c7ed14376a254
mjyazar <mjanjua.ed@gmail.com>
2026-03-31 19:07:41 +0200

    run terminal simulation

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import config
+import simulation.terminal as terminal
 from amplifier.sr830 import SR830
 
 
 def main():
     
     with SR830(config.INTERFACE, backend=config.BACKEND, timeout_ms=config.TIME_OUT_MS) as amp:
-    
-        print("Frequency: ", amp.frequency())
+        return terminal.simulation(amp)
         
 
 if __name__ == "__main__":
