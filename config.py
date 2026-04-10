7443566573db10e74551f00ff9e2ef724d7106f9
mjyazar <mjanjua.ed@gmail.com>
2026-04-10 09:55:35 +0200

    lab testing ready config

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 # Connection settings for SR830 lock-in amplifier
 
 # argument '' on lab computer, '@py' if using pyvisa-py (if no NI-VISA installed)   
-BACKEND = 'simulation/simulation.yaml@sim'
+BACKEND = ''
 
 # change to match resource name on amplifier, e.g. "GPIB0::1::INSTR" instead of "GPIB0::8::INSTR"
-INTERFACE = 'GPIB::1::INSTR'
+INTERFACE = 'GPIB::8::INSTR'
 
 # any operation longer than this will raise a timeout error (in milliseconds)
 TIME_OUT_MS = 10000 
